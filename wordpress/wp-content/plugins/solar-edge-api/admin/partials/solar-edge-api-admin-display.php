fa781182c3fdda6d82ce62e56aa1d5f890f3a4cd
Rosaan Ramasamy <rosaan@blazesolutions.com.my>
2020-04-02 19:24:42 +0800

    first init

---
+++ wordpress/wp-content/plugins/solar-edge-api/admin/partials/solar-edge-api-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://rosaan.com/
+ * @since      1.0.0
+ *
+ * @package    Solar_Edge_Api
+ * @subpackage Solar_Edge_Api/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ wordpress/wp-content/plugins/solar-edge-api/includes/class-solar-edge-api-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://rosaan.com/
+ * @since      1.0.0
+ *
+ * @package    Solar_Edge_Api
+ * @subpackage Solar_Edge_Api/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Solar_Edge_Api
+ * @subpackage Solar_Edge_Api/includes
+ * @author     Rosaan <rosaan@blazesolutions.com.my>
+ */
+class Solar_Edge_Api_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
